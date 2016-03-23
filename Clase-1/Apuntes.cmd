f983024d5692c39f205682a0c214fd4f1a38b88d
Roykssop <herna14@gmail.com>
2016-03-22 21:05:18 -0300

    Create Apuntes.cmd

---
+++ Clase-1/Apuntes.cmd
@@ -0,0 +1,43 @@
+Profesor
+
+Ernestogigliotti@gmail.com
+
+14 clases
+
+Clase 8 : Exámen
+
+Ide: Android Studio
+Wizard para la instalación
+
+Conceptos
+
+Un activity representa una pantalla 
+Esta compuesto por:
+  clase java asociada (.java). 
+  un layout (xml).
+  
+  
+  Carpeta java
+    Tiene nuestro código y los paquetes
+  Carpeta Res
+    Son los recursos, archivos fijos que usa la app (imágenes, layout, sonido, etc)
+    
+    Carpeta drawable
+      Van las imágenes
+    
+    Carpeta Layout
+      Van los archivos de layout
+    
+    Carpeta mipmap
+      Ícono de la app
+      
+    Carpeta Values
+      Colores, strings, estilos.
+      
+      
+    Graddle 
+      Herramienta de compilación.
+    
+    
+    
+      
